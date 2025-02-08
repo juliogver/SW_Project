ca0e921a343a64257c7870e3be384472b570a049
juliogver <jules.gravier@gmail.com>
2025-02-08 17:29:37 +0100

    ajout de parametres dans convert_to_rdf

--- scripts/convert_csv_to_rdf.py
+++ scripts/convert_csv_to_rdf.py
@@ -13,13 +13,26 @@ EX = Namespace("http://myproject.org/ai_ontology#")
 # Ajouter des articles au graphe
 for _, row in df.iterrows():
     article_uri = URIRef(EX[f"Article_{row['ID']}"])
+    author_uri = URIRef(EX[f"Author_{row['Article Author Name'].replace(' ', '_')}"])  # Pour créer un URI propre
+    domain_uri = Literal(row["Article Main Domain"])
     
+    # Ajout des propriétés de base
     g.add((article_uri, EX.hasTitle, Literal(row["Article Title"])))
-    g.add((article_uri, EX.hasAuthor, Literal(row["Article Author Name"])))
+    g.add((article_uri, EX.hasAuthor, author_uri))  # URI pour l'auteur
     g.add((article_uri, EX.hasDate, Literal(row["Article Date"])))
     g.add((article_uri, EX.hasContent, Literal(row["Article Content"])))
+    g.add((article_uri, EX.hasDomain, domain_uri))
+
+    # Ajout de la relation entre l'auteur et l'article
+    g.add((author_uri, EX.wroteArticle, article_uri))
+
+    # Ajout des sujets (topics)
+    topics = row["Article Topics"].split(", ")  # Séparation des sujets par virgule
+    for topic in topics:
+        topic_uri = URIRef(EX[f"Topic_{topic.replace(' ', '_')}"])  # Créer un URI propre pour le topic
+        g.add((article_uri, EX.hasTopic, topic_uri))
 
 # Sauvegarde du graphe RDF
 g.serialize("../data/rdf_data.ttl", format="turtle")
 
-print("Le fichier data.rdf a été généré avec succès !")
+print("Le fichier rdf_data.ttl a été généré avec succès !")
